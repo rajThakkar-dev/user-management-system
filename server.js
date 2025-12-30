2c747aaf04660fd51ea39cf622c442a855a5b8e9
rajThakkar-dev <rajrajde1014@gmail.com>
2025-12-31 00:46:03 +0530

    fix: mistakes fixed

--- server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-const connectDB = require("./src/config/db");
-
-connectDB();
-app.listen(process.env.PORT || 5000, () =>
-    console.log("Server running")
-);
